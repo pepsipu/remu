cbe11f9f8e9882207fb2f2ed01fbeabcd0c656f5
Sammy Hajhamid <pokeso123@hotmail.com>
2020-07-23 17:04:58 -0700

    wasm loading & inst

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { ResultObject } from '@assemblyscript/loader';
 import './App.css';
 
-function App() {
+function App(props: { wasm: ResultObject }) {
+  const { wasm } = props;
+  // @ts-ignore because ts wont know the exports at compile time
+  const wasmFns: WasmExports = wasm.instance.exports;
   return (
-    <></>
+    <p>
+      {wasmFns.add(1, 2)}
+    </p>
   );
+}
+
+interface WasmExports {
+  add: (a: number, b: number) => number
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { instantiateStreaming } from '@assemblyscript/loader';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+instantiateStreaming(fetch('./remu.wasm')).then((wasm) => {
+  ReactDOM.render(
+    <App wasm={wasm} />,
+    document.getElementById('root'),
+  );
+});

8daf9b25050cc3930236ed4443cd184c956cef3e
Sammy Hajhamid <pokeso123@hotmail.com>
2020-07-25 00:18:39 -0700

    instruction length decode

--- asm/assembly/cpu.ts
+++ asm/assembly/cpu.ts
@@ -6,6 +6,32 @@ class CPU {
   private regs: StaticArray<usize> = new StaticArray(32);
 
   public mmu: MMU = new MMU();
+
+  public step = () => {
+    const instruction = this.mmu.read<u16>(this.pc);
+  };
+
+  // refer to page 6 of the riscv spec v2.2 to find the instruction size diagram
+  // returns how many bytes the instruction is
+  public getInstructionSize = (instruction: u16): usize => {
+    if ((instruction & 0x3) === 0x3) {
+      if ((instruction & 0x1f) === 0x1f) {
+        if ((instruction & 0x3f) !== 0x1f) {
+          if (((instruction & 0x7f) !== 0x3f)) {
+            const offsetSize = instruction & 0x7000;
+            if (offsetSize === 0x7000) {
+              abort('wtf type of instruction did u give me');
+            }
+            return 10 + offsetSize * 2;
+          }
+          return 8;
+        }
+        return 6;
+      }
+      return 4;
+    }
+    return 2;
+  };
 }
 
 const cpu: CPU = new CPU();
--- asm/assembly/mmu.ts
+++ asm/assembly/mmu.ts
@@ -4,10 +4,12 @@ const MEM_SIZE: i32 = 134217728;
 export default class MMU {
   private mem: StaticArray<u8> = new StaticArray<u8>(MEM_SIZE);
 
+  @inline()
   read<T>(address: usize): T {
     return load<T>(this.getMemoryPtr() + address);
   }
 
+  @inline()
   write<T>(address: usize, value: T): void {
     store<T>(this.getMemoryPtr() + address, value);
   }
